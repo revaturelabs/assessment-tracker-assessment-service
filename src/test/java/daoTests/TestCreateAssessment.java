c1dd3b4ea56405ef69c767948784fc5b353ba106
irfreshour <47945235+irfreshour@users.noreply.github.com>
2021-07-08 16:26:50 -0400

    refactored TestCreateAssessment tests

--- src/test/java/daoTests/TestCreateAssessment.java
+++ src/test/java/daoTests/TestCreateAssessment.java
@@ -2,86 +2,88 @@ package daoTests;
 
 import dao.AssessmentDAOImpl;
 import models.Assessment;
-import models.Grade;
-import util.ConnectionDB;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCreateAssessment {
     // Class to be tested
     private AssessmentDAOImpl adao;
 
-    // Dependencies
-    private Connection mockConn;
-    private Grade mockGrade;
-    private PreparedStatement mockPs;
-    private ResultSet mockRs;
-
     // Test Data
-    private Assessment sampleAssessment;
-    private Assessment sa1,sa2,sa3;
+    private Assessment sa1, sa2, sa3, sa4;
 
     @Before
     public void setup() throws Exception {
-        // Create our Mock objects
-        mockConn  = Mockito.mock(Connection.class);
-        mockGrade = Mockito.mock(Grade.class);
-        mockPs    = Mockito.mock(PreparedStatement.class);
-        mockRs    = Mockito.mock(ResultSet.class);
-
-        // Since getconnection is a static method, get a static mock object
-        try (MockedStatic<ConnectionDB> mockedStatic = Mockito.mockStatic(ConnectionDB.class)) {
-            mockedStatic.when(ConnectionDB::getConnection).thenReturn(mockConn);
-        }
-
-        // When prepareStatement is called on the connection, return the prepared statement
-        // When executeQuery is called, return the result set
-        Mockito.when(mockConn.prepareStatement(Mockito.any(String.class))).thenReturn(mockPs);
-        Mockito.when(mockPs.executeQuery()).thenReturn(mockRs);
-
-
         // Initialize the class to be tested
         adao = new AssessmentDAOImpl();
 
         ArrayList<String> sampleNote = new ArrayList<String>();
         sampleNote.add("Needs Improvement");
 
-        // Create a sample assessment", 1,
-        sampleAssessment = new Assessment(1, "Test Assessment", 1, 3, "3", 30, 4,sampleNote);
-        sa1 = new Assessment(2,"Test 1",1,3,"5",20,1,sampleNote);
+        // Create sample assessments"
+        sa1 = new Assessment(0, "Delete later", 1, 3, "5", 20, 1, sampleNote);
+        sa2 = new Assessment(0, "Delete later", 2, 4, null, 2, 2, sampleNote);
+        sa3 = new Assessment(0, null, 3, 4, "6", 22, 3, sampleNote);
     }
 
     @Test
-    public void testNotNull() throws Exception {
-        Assessment returnedAssessment = adao.createAssessment(sampleAssessment);
-        assertNotNull(returnedAssessment);
-    }
-
-    @Test
-    public void testCorrectBatchIdSet() throws Exception {
+    public void testCreateAssessmentPass() {
         Assessment returnedAssessment = adao.createAssessment(sa1);
         assertNotNull(returnedAssessment);
+    }
+
+    @Test
+    public void testCorrectAssessmentTitlePass() {
+        Assessment returnedAssessment = adao.createAssessment(sa1);
+        assertEquals("Delete later", returnedAssessment.getAssessmentTitle());
+    }
+
+    @Test
+    public void testCorrectTypeIdPass() {
+        Assessment returnedAssessment = adao.createAssessment(sa1);
+        assertEquals(1, returnedAssessment.getTypeId());
+    }
+
+    @Test
+    public void testCorrectBatchIdPass() {
+        Assessment returnedAssessment = adao.createAssessment(sa1);
         assertEquals(3, returnedAssessment.getBatchId());
     }
 
     @Test
-    public void testCorrectWeekIdSet() throws Exception {
+    public void testCorrectWeekIdPass() {
         Assessment returnedAssessment = adao.createAssessment(sa1);
-        assertNotNull(returnedAssessment);
         assertEquals("5", returnedAssessment.getWeekId());
+    }
+
+    @Test
+    public void testCorrectAssessmentWeightPass() {
+        Assessment returnedAssessment = adao.createAssessment(sa1);
+        assertEquals(20, returnedAssessment.getAssessmentWeight());
+    }
+
+    @Test
+    public void testCorrectCategoryIdPass() {
+        Assessment returnedAssessment = adao.createAssessment(sa1);
+        assertEquals(1, returnedAssessment.getCategoryId());
+    }
+
+
+    @Test
+    public void testNullWeekIdFail() {
+        Assessment returnedAssessment = adao.createAssessment(sa2);
+        assertNull(returnedAssessment);
+    }
+
+    @Test
+    public void testNullAssessmentTitleFail() {
+        Assessment returnedAssessment = adao.createAssessment(sa3);
+        assertNull(returnedAssessment);
     }
 
 }
