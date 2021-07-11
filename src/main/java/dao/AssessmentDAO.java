499239987d9a7ac22b1fad844b29120a60f16877
rtaylor4444 <rtaylor4444@gmail.com>
2021-07-11 00:49:45 -0400

    more fixes

--- src/main/java/dao/AssessmentDAO.java
+++ src/main/java/dao/AssessmentDAO.java
@@ -2,29 +2,43 @@ package dao;
 
 import exceptions.InvalidValue;
 import exceptions.ResourceNotFound;
-import exceptions.ResourceUnchangable;
 import models.Assessment;
+import models.Grade;
+import models.Note;
+import models.AssessmentType;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface AssessmentDAO {
 
-     Assessment createAssessment(Assessment assessment);
+     Assessment createAssessment(Assessment a) throws InvalidValue;
+
+     Assessment getAssessmentById(int assessmentId) throws ResourceNotFound, InvalidValue;
 
      List<Assessment> getAssessments();
-
-     Assessment getAssessment(int id) throws ResourceNotFound;
-
-     Assessment updateAssessment(Assessment assessment) throws ResourceNotFound, ResourceUnchangable;
-
-     void deleteAssessment(int id) throws ResourceNotFound, ResourceUnchangable;
-
-     List<Assessment> getAssessmentsByAssociateId(int associateId);
-
-     // List<Note> getNotesForTrainee(int id, int weekId);
 
      boolean adjustWeight(int assessmentId, int weight) throws InvalidValue, ResourceNotFound;
 
      boolean assignAssessmentType(int assessmentId, int typeId) throws SQLException, ResourceNotFound, InvalidValue;
+
+     boolean deleteAssessment(int assessmentId) throws ResourceNotFound;
+
+     List<Assessment> getAssessmentsByTraineeId(int traineeId);
+
+     List<Assessment> getBatchWeek(int batchId, String weekId);
+
+     //BUG - To extract
+     List<Grade> getGradesForWeek(int traineeId, String weekId);
+
+     AssessmentType createAssessmentType(String name, int defaultWeight);
+
+     List<Note> getNotesForTrainee(int id, int weekId);
+
+     Grade insertGrade(Grade grade);
+
+     Grade getGradeForAssociate(int associateId, int assessmentId);
+
+     Grade updateGrade(Grade grade);
+
 }
