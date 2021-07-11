894a83504bb6989678bcf2fc8e4dac71f9879931
rtaylor4444 <rtaylor4444@gmail.com>
2021-07-10 23:21:50 -0400

    fixed more tests

--- src/test/java/daoTests/TestCreateAssessmentType.java
+++ src/test/java/daoTests/TestCreateAssessmentType.java
@@ -6,20 +6,12 @@ import exceptions.DuplicateResource;
 import exceptions.ResourceNotFound;
 import exceptions.ResourceUnchangable;
 import models.AssessmentType;
-import models.Grade;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
-import util.ConnectionDB;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,7 +28,7 @@ public class TestCreateAssessmentType {
     @Test
     public void testCreateAssessmentType() {
         try {
-            AssessType1 = new AssessmentType(0, "testAssessmentType", 50);
+            AssessType1 = new AssessmentType(0, "Test", 50);
             AssessType1 = assessmentTypeDAO.createAssessmentType(AssessType1);
             Assert.assertTrue(AssessType1.getTypeId() != 0);
         } catch(DuplicateResource e) {
@@ -47,7 +39,7 @@ public class TestCreateAssessmentType {
     @Test
     public void testCreateDupAssessmentType() {
         try {
-            AssessType2 = new AssessmentType(0, "testAssessmentType2", 50);
+            AssessType2 = new AssessmentType(0, "Code", 50);
             AssessType2 = assessmentTypeDAO.createAssessmentType(AssessType2);
             assessmentTypeDAO.createAssessmentType(AssessType2);
             fail();
@@ -59,8 +51,8 @@ public class TestCreateAssessmentType {
     @AfterClass
     public static void cleanup() {
         try {
-            if(AssessType1 != null) assessmentTypeDAO.deleteAssessmentType(AssessType1.getTypeId());
-            if(AssessType2 != null) assessmentTypeDAO.deleteAssessmentType(AssessType2.getTypeId());
+            assessmentTypeDAO.deleteAssessmentType(AssessType1.getTypeId());
+            assessmentTypeDAO.deleteAssessmentType(AssessType2.getTypeId());
         } catch(ResourceNotFound | ResourceUnchangable e) {
             //Should not get here
             fail();
